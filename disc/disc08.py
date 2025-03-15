7d77490195479f7d444c91ef588af9056ce9fb15
Yongheng Liu <161477977+huahai27@users.noreply.github.com>
2025-03-15 16:02:23 +0800

    Did disc08

---
+++ disc/disc08.py
@@ -0,0 +1,127 @@
+def strange_loop():
+    """Return a Link s for which s.rest.first.rest is s.
+
+    >>> s = strange_loop()
+    >>> s.rest.first.rest is s
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    s = Link(1);
+    last = Link(1, s);
+    mid = Link(last);
+    s.rest = mid;
+    return s
+
+
+def sum_rec(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_rec(a, 2)
+    7
+    >>> sum_rec(a, 5)
+    15
+    >>> sum_rec(Link.empty, 1)
+    0
+    """
+    # Use a recursive call to sum_rec; don't call sum_iter
+    "*** YOUR CODE HERE ***"
+    if k == 0 or s is Link.empty:
+        return 0
+    else:
+        return s.first + sum_rec(s.rest, k - 1)
+    
+def sum_iter(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_iter(a, 2)
+    7
+    >>> sum_iter(a, 5)
+    15
+    >>> sum_iter(Link.empty, 1)
+    0
+    """
+    # Don't call sum_rec or sum_iter
+    "*** YOUR CODE HERE ***"
+    sum = 0
+    cnt = 0
+    while k != 0 and cnt < k and s is not Link.empty:
+        sum += s.first
+        s = s.rest
+        cnt += 1
+    return sum
+
+
+def overlap(s, t):
+    """For increasing s and t, count the numbers that appear in both.
+
+    >>> a = Link(3, Link(4, Link(6, Link(7, Link(9, Link(10))))))
+    >>> b = Link(1, Link(3, Link(5, Link(7, Link(8)))))
+    >>> overlap(a, b)  # 3 and 7
+    2
+    >>> overlap(a.rest, b)  # just 7
+    1
+    >>> overlap(Link(0, a), Link(0, b))
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    cnt = 0
+    while s is not Link.empty and t is not Link.empty:
+        if s.first > t.first:
+            t = t.rest
+        elif s.first < t.first:
+            s = s.rest
+        else:
+            t = t.rest
+            s = s.rest
+            cnt += 1
+    return cnt
+
+
+def display(s, k=10):
+    """Print the first k digits of infinite linked list s as a decimal.
+
+    >>> s = Link(0, Link(8, Link(3)))
+    >>> s.rest.rest.rest = s.rest.rest
+    >>> display(s)
+    0.8333333333...
+    """
+    assert s.first == 0, f'{s.first} is not 0'
+    digits = f'{s.first}.'
+    s = s.rest
+    for _ in range(k):
+        assert s.first >= 0 and s.first < 10, f'{s.first} is not a digit'
+        digits += str(s.first)
+        s = s.rest
+    print(digits + '...')
+    
+def divide(n, d):
+    """Return a linked list with a cycle containing the digits of n/d.
+
+    >>> display(divide(5, 6))
+    0.8333333333...
+    >>> display(divide(2, 7))
+    0.2857142857...
+    >>> display(divide(1, 2500))
+    0.0004000000...
+    >>> display(divide(3, 11))
+    0.2727272727...
+    >>> display(divide(3, 99))
+    0.0303030303...
+    >>> display(divide(2, 31), 50)
+    0.06451612903225806451612903225806451612903225806451...
+    """
+    assert n > 0 and n < d
+    result = Link(0)  # The zero before the decimal point
+    cache = {}
+    tail = result
+    while n not in cache:
+        q, r = 10 * n // d, 10 * n % d
+        tail.rest = Link(q)
+        tail = tail.rest
+        cache[n] = tail
+        n = r
+    tail.rest = cache[n]
+    return result
+
